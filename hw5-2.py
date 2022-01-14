f5e235fbf8e8720b7f46c633ec900d78875d74a9
p22jiaropoli <p22jiaropoli@fairfieldprep.org>
2022-01-14 10:01:07 -0500

    Add file

--- hw5-2.py
+++ hw5-2.py
@@ -1,2 +1,45 @@
 # Author JRI 11/2/21
+import math
 
+pi = math.pi
+e = math.e
+
+# Question 1
+a = math.isqrt(529)
+print(a)
+
+# Quesrion 2
+b = math.sin(0)
+print(b)
+
+# Quesrion 3
+c = math.cos(2 * pi)
+print(c)
+
+# Question 4
+d = math.pow(2.5, 5)
+print(d)
+
+# Question 5
+e = math.log(e)
+print(e)
+
+# Question 6
+f = math.pow(e, 4)
+print(f)
+
+# Question 7
+g = math.ceil(-12.9)
+print(g)
+
+# Question 8
+h = math.floor(7.25)
+print(h)
+
+# Question 9
+i = (2 * (math.log(1000)))
+print(i)
+
+# Question 10
+j = math.radians(180)
+print(j)
